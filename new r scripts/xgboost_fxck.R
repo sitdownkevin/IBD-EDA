5be6797f0f36cba3e7b4cd127f45bdc34b3f0f5f
XU Ke <kexu567@gmail.com>
2024-04-02 17:29:33 +0800

    update

--- new r scripts/xgboost_fxck.R
+++ new r scripts/xgboost_fxck.R
@@ -1,7 +1,9 @@
+# 清除变量
+# 设置工作环境
 rm(list=ls(all=TRUE))
 setwd('~/GitHub/IBD-EDA')
 
-
+# 加载软件包
 library(dplyr)
 library(xgboost)
 library(performance)
@@ -9,7 +11,7 @@ library(ggplot2)
 library(corrplot)
 library(DALEX)
 
-
+# 读取数据
 read.csv('./new r scripts/data_norm.csv') %>%
   select(-X) -> data
 
@@ -18,7 +20,8 @@ data %>% str
 # 设置数据和参数
 dtrain <- xgb.DMatrix(data = as.matrix(data[,-1]), label = data[,1])
 
-# 设置参数，这里参数和您之前设置的一样
+
+# 设置参数 (已经选出了 XGBoost 的最佳参数)
 params <- list(
   objective = "reg:squarederror",
   max_depth = 4,
@@ -29,33 +32,45 @@ params <- list(
   colsample_bytree = 0.8
 )
 
+
 # 使用xgb.cv进行交叉验证
-cv.nfold <- 8
-cv.result <- xgb.cv(params = params, data = dtrain, nrounds = 100, nfold = cv.nfold, 
-                    metrics = list("rmse"), 
-                    print_every_n = 10, early_stopping_rounds = 10, 
-                    maximize = FALSE)
+# xgb.cv(params = params, 
+#        data = dtrain, 
+#        nrounds = 100, 
+#        nfold = 10, 
+#        metrics = list("rmse"), 
+#        print_every_n = 10, 
+#        early_stopping_rounds = 10, 
+#        maximize = FALSE) -> 
+#   cv.result
+
 
 # 查看交叉验证的结果
-print(cv.result)
+# print(cv.result)
+
 
 # 使用全部数据训练最终模型
-final_model <- xgboost(data = dtrain, params = params, nrounds = 100, 
-                     print_every_n = 10, early_stopping_rounds = 10)
+xgboost(data = dtrain, 
+        params = params,               
+        nrounds = 100, 
+        print_every_n = 10, 
+        early_stopping_rounds = 10) -> 
+  final_model
+
+
+# 计算R^2
+actuals <- data[, 1] # 实际值
+preds <- predict(final_model, newdata = as.matrix(data[,-1])) # 预测值
+
+SST <- sum((actuals - mean(actuals))^2)
+SSR <- sum((preds - actuals)^2)
+
+r2_value <- 1 - SSR/SST
+
+print(paste("R^2 Value: ", r2_value))
+
 
 
 # 计算特征重要性
 importance_matrix <- xgb.importance(feature_names = colnames(data[,-1]), model = final_model)
 importance_plot <- xgb.plot.importance(importance_matrix)
-# ggsave("feature_importance.pdf", importance_plot)
-
-
-# 计算R^2
-actuals <- data[,1] # 实际值
-preds <- predict(final_model, newdata = as.matrix(data[,-1])) # 预测值
-
-SST <- sum((actuals - mean(actuals))^2)
-SSR <- sum((preds - actuals)^2)
-r2_value <- 1 - SSR/SST
-
-print(paste("R^2 Value: ", r2_value))
